44107fc0690903dc7175507595d989ff19709c26
ccchibritoss <nikitabazik2@gmail.com>
2025-10-24 23:43:47 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,16 +1,12 @@
 // config.js
 const CONFIG = {
     ADMIN_ID: "7773057702",
-    BOT_USERNAME: "RentalNftBot",
+    BOT_USERNAME: "RentalNftBot", // Твой бот @RentalNftBot
     
     // Supabase
     SUPABASE_URL: "https://gzwctaavbdsorfonlqgo.supabase.co",
     SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imd6d2N0YWF2YmRzb3Jmb25scWdvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjEyMzkyNTAsImV4cCI6MjA3NjgxNTI1MH0.p1gDOZUmdAgi_9tDaNZvg6xdBH_N-PKhmtpWaMuaMko",
     
-    // Bot commands
-    BOT_COMMANDS: {
-        GIFT: "/gift",
-        STATS: "/stats", 
-        GIFTS: "/gifts"
-    }
+    // Bot links
+    BOT_LINK: "https://t.me/RentalNftBot"
 };
